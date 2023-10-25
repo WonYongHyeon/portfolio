3cf1be67f35cffee47fc1d46a05c74bd464cf449
WonYongHyeon <quin1392@gmail.com>
2023-10-25 21:53:15 +0900

    eslint 예외 적용

--- src/component/header/header.container.tsx
+++ src/component/header/header.container.tsx
@@ -11,6 +11,7 @@ export default function Header() {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleScroll = useMemo(
